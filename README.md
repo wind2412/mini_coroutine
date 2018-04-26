8feefffce3e9c2b9d0726cb0b0089622758a19da
wind2412 <944797358@qq.com>
2018-04-27 03:07:09 +0800

    init.
    typo... maybe I can go to sleep now.

---
+++ README.md
@@ -0,0 +1,10 @@
+# A toy coroutine for fun
+
+Only a toy to present the principle of coroutine. It's flavor is some what like lua coroutine... (but without arguments, very very simple but I would like to add it some other day)
+
+# Play:
+Maybe on linux only (mac cannot link. windows I dont know).
+```
+g++ -std=c++11 -g switch_to.S simple_coro.cc -o simple_coro
+./simple_coro
+```
---
+++ simple_coro.cc
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+/*===--------------- definition ------------------===*/
+
+#define PAGE_SIZE 1024
+#define STACK_SIZE (2 * PAGE_SIZE)
+
+typedef uint64_t u64;
+typedef uint8_t u8;
+
+struct Context {	// callee save registers
+	u64 rbp = 0;
+	u64 rbx = 0;
+	u64 r12 = 0;
+	u64 r13 = 0;
+	u64 r14 = 0;
+	u64 r15 = 0;
+	
+	u64 rsp = 0;
+	u64 rip = 0;
+};
+
+typedef void (*fp)();
+
+class Coro {
+private:
+	Context cxt;
+	Context *from;
+	u8 stack[STACK_SIZE];
+	fp closure;				// task.
+public:
+	Coro(fp closure = nullptr) : from(nullptr), closure(closure) {		// [[noreturn]]
+		if (closure == nullptr) return;
+		cxt.rsp = (u64)(stack + STACK_SIZE);
+		cxt.rip = (u64)closure;
+	}
+private:
+	void switch_to(Context *other) { switchTo(other, &this->cxt); }
+public:
+	void yield() 				  { switch_to(this->from); }								// go back to the caller
+	void resume(Coro *other)	  { other->from = &this->cxt; switch_to(&other->cxt); }		// go to one `coroutine` except `the main thread`
+};
+
+extern "C" void switchTo(Context *, Context *);
+
+/*===-------------- implementations -----------------====*/
+
+Coro main_coro;			// the main thread's context. It is very important.
+
+/*===---------------- test ---------------------===*/
+
+Coro *first;
+Coro *second;
+
+int main(int argc, char *argv[]) 
+{
+	first = new Coro([](){
+		cout << "haha" << endl;
+		first->resume(second);
+		first->yield();
+		cout << "hehe" << endl;
+	});
+	second = new Coro([](){
+		cout << "hoho" << endl;
+		second->yield();
+		cout << "heihei" << endl;
+	});
+	
+	main_coro.resume(first);
+	
+	cout << "over." << endl;
+}
